f66e9c02418cf4d964d6ea06482df6f7532ae2fa
Rafael Schouten <rafaelschouten@gmail.com>
2014-01-24 17:50:58 +1100

    revert node title

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -77,8 +77,22 @@
 ?>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <?php if (!$page): ?>
+      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    <?php endif; ?>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
 
-
+  <?php if ($display_submitted): ?>
+    <div class="posted">
+      <?php if ($user_picture): ?>
+        <?php print $user_picture; ?>
+      <?php endif; ?>
+      <?php print $submitted; ?>
+    </div>
+  <?php endif; ?>
 
   <?php
     // We hide the comments and links now so that we can render them later.
@@ -90,15 +104,6 @@
 
   <?php if (!empty($content['field_tags']) && !$is_front): ?>
     <?php print render($content['field_tags']) ?>
-  <?php endif; ?>
-
-  <?php if ($display_submitted): ?>
-    <div class="posted">
-      <?php if ($user_picture): ?>
-        <?php print $user_picture; ?>
-      <?php endif; ?>
-      <?php print $submitted; ?>
-    </div>
   <?php endif; ?>
 
   <?php print render($content['links']); ?>
