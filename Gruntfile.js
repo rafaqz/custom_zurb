295c1a8a0995b60603fad14084cf5813f8aa86b2
Rafael Schouten <rafaelschouten@gmail.com>
2015-03-11 11:03:47 +1100

    bugfix grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 		watch: {
 			sass: {
 				files: ['scss/**/*.{scss,sass}'],
-				tasks: ['sass']
+				tasks: ['sass:dist']
 			},
 			livereload: {
 				files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
@@ -20,12 +20,12 @@ module.exports = function(grunt) {
 			},
 			dist: {
 				files: {
-					'css/custom_zurb.css': 'sass/custom_zurb.scss'
+					'app/styles.css': 'sass/app.scss'
 				}
 			}
 		}
 	});
-	grunt.registerTask('default', ['sass', 'watch']);
+	grunt.registerTask('default', ['sass:dist', 'watch']);
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 };
